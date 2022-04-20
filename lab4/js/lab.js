9c9a68670b0709b5fd509a771049fae7dbce650c
MYLES <mrideral@ucsc.edu>
2022-04-20 11:40:13 -0700

    lab4 lab5 lab2 upload

---
+++ lab4/js/lab.js
@@ -0,0 +1,20 @@
+// Summary. (use period)
+// Description. (use period)
+// @link   URL
+// @file   This files defines the MyClass class.
+// @author AuthorName.
+// @since  x.x.x
+
+
+// Connect 4
+// 1. Print a 6 x 7 board of circle slots
+// 2. Accept user input
+//   a. Detect mouse for player selection
+//   b.Determine acceptable input
+//     i.Each turn must land on the bottom level of the board or on top of a piece placed during a previous turn
+//   c.Confirm player selection
+// 3.Send updated board to server ending players turn
+// 4.Check win or draw condition(after the 8th turn)
+// 5.Computer evaluates possible moves
+// 6.Computer chooses and confirms selection
+// 7.Send updated board to server ending computers turn
---
+++ lab5/js/lab.js
@@ -0,0 +1,20 @@
+/*
+* Author: Myles Rider Alexis
+* Created 4/20
+*
+*/
+
+var make = "Tesla";
+var model = "Model S";
+var carYear = 2022;
+var color = "Purple";
+var ownIt = false;
+var currantYear = 2022;
+var carAge = currantYear - carYear;
+
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Year: " + carYear + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Own: " + ownIt + "<br>");
+document.writeln("Age: " + carAge + "<br>");
